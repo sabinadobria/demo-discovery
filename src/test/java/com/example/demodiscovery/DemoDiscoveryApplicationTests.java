9d24a8c853df1df1a652c8263af181af81caf3a5
Maria Dobria <maria.dobria@1and1.ro>
2020-06-05 15:45:20 +0300

    Initial commit

---
+++ src/test/java/com/example/demodiscovery/DemoDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demodiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
